b9a65ca0a051f3905379539686b3d02c2a84e3a9
KIM MIN BO <68101836+minbo2002@users.noreply.github.com>
2022-07-31 23:37:56 +0900

    Book 엔티티, Controller, Service, Repository, Dto 생성

---
+++ src/main/java/com/example/junitproject/domain/Book.java
@@ -0,0 +1,31 @@
+package com.example.junitproject.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String title;
+
+    @Column(length = 20, nullable = false)
+    private String author;
+
+    @Builder
+    public Book(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+}
---
+++ src/main/java/com/example/junitproject/dto/BookRespDto.java
@@ -0,0 +1,4 @@
+package com.example.junitproject.dto;
+
+public class BookRespDto {
+}
