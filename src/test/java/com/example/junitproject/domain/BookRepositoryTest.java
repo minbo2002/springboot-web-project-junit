f3942897c219fdf9741618125c889a9e90b99726
KIM MIN BO <68101836+minbo2002@users.noreply.github.com>
2022-07-31 23:51:18 +0900

    Repository 테스트 환경 세팅

--- src/test/java/com/example/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/com/example/junitproject/domain/BookRepositoryTest.java
@@ -1,7 +1,25 @@
 package com.example.junitproject.domain;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class BookRepositoryTest {
+
+    /*
+    1.책 등록  2.책 목록  3.책 한건조회  4.책 수정  5.책 삭제
+     */
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    public void 책등록_test() {
+        System.out.println("책등록_test 실행");
+    }
+
 
 }
